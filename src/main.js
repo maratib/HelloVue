2baea8f429b50ae3572788774a221c157e9ba77a
Maratib Ali Khan <maratib@gmail.com>
2020-02-16 01:25:58 +0500

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import 'bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/assets/scss/app.scss';
+
 
 // bootstrapVue
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
